21673ded8ea53775b7ddc94ce2d0af775b700c5d
iskylarkus <turgay.kibar@gmail.com>
2022-04-18 19:11:29 +0300

    subscriber developed

--- RabbitMQNet5.Subscriber/Program.cs
+++ RabbitMQNet5.Subscriber/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMQNet5.Subscriber
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://efijhuug:yCOtUP8zIYnjfJlj4jTyLk9t6vSF8uTF@toad.rmq.cloudamqp.com/efijhuug");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                string queue = "hello-queue";
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                channel.BasicConsume(queue, true, consumer);
+
+                consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+                {
+                    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                    Console.WriteLine("Received Message: " + message);
+                };
+
+                Console.ReadLine();
+            }
         }
     }
 }
