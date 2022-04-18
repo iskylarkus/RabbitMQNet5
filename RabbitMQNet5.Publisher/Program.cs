26aac671c1e81f2c5a681f1f82e085756a635e9a
iskylarkus <turgay.kibar@gmail.com>
2022-04-18 19:11:12 +0300

    publisher developed

--- RabbitMQNet5.Publisher/Program.cs
+++ RabbitMQNet5.Publisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQNet5.Publisher
 {
@@ -6,7 +8,28 @@ namespace RabbitMQNet5.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://efijhuug:yCOtUP8zIYnjfJlj4jTyLk9t6vSF8uTF@toad.rmq.cloudamqp.com/efijhuug");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                string queue = "hello-queue";
+
+                channel.QueueDeclare(queue, true, false, false);
+
+                string message = "Hello World > " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, queue, null, messageBody);
+            }
+
+            Console.WriteLine("Send the message...");
+
+            Console.ReadLine();
         }
     }
 }
