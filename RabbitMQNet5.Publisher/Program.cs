7c5ab2411d42128fe78099edbe047bcaf73e83d1
iskylarkus <turgay.kibar@gmail.com>
2022-04-18 18:35:19 +0300

    Publisher project created

---
+++ RabbitMQNet5.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQNet5.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
