2a0ebd1b90690cd6f6e7177e11cd1a96a0f94a54
iskylarkus <turgay.kibar@gmail.com>
2022-04-18 18:36:24 +0300

    Subscriber project created

---
+++ RabbitMQNet5.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQNet5.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
