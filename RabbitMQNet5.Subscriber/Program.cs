424be71cecf649861d3bb7a5e4ec24151fceca26
iskylarkus <turgay.kibar@gmail.com>
2022-04-19 09:58:48 +0300

    BasicAck and BasicQos implemented with adjusting autoAck for many consumers

--- RabbitMQNet5.Subscriber/Program.cs
+++ RabbitMQNet5.Subscriber/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace RabbitMQNet5.Subscriber
 {
@@ -9,6 +10,9 @@ namespace RabbitMQNet5.Subscriber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please press enter key to start...");
+            Console.ReadLine();
+
             var factory = new ConnectionFactory();
 
             factory.Uri = new Uri("amqps://efijhuug:yCOtUP8zIYnjfJlj4jTyLk9t6vSF8uTF@toad.rmq.cloudamqp.com/efijhuug");
@@ -19,15 +23,21 @@ namespace RabbitMQNet5.Subscriber
 
                 string queue = "hello-queue";
 
+                channel.BasicQos(0, 1, false);
+
                 var consumer = new EventingBasicConsumer(channel);
 
-                channel.BasicConsume(queue, true, consumer);
+                channel.BasicConsume(queue, false, consumer);
 
                 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
                 {
                     var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
                     Console.WriteLine("Received Message: " + message);
+
+                    channel.BasicAck(e.DeliveryTag, false);
+
+                    Thread.Sleep(1000);
                 };
 
                 Console.ReadLine();
